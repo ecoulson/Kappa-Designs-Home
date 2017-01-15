dfaedfe12c6bef20bdb9dda89d498f004dce4fc6
ecoulson <ivansonofcoul@gmail.com>
2017-01-14 20:58:46 -0800

    My son Evan is A TURD

---
+++ js/client.js
@@ -0,0 +1,65 @@
+function Title(words) {
+  this.words = words;
+  this.index = 0;
+  this.$titleContainer = $('.sliding-title-container');
+  this.$titleInner = $('.sliding-title-inner');
+  this.$titleBorder = $('.sliding-title-border');
+  this.$titleBorder.height($('.sliding-title-container').height());
+  this.$titleInner.text(this.words[this.index]);
+
+  this.slide = function () {
+    if (this.index == this.words.length) {
+      this.index = 0;
+    }
+    this.$titleBorder.css({
+      'transition':'1s padding-left ease-in-out',
+      'padding-top':'10px',
+      'padding-bottom':'10px',
+      'padding-left':0
+    })
+
+    setTimeout(function () {
+      this.$titleInner.text('');
+    }.bind(this), 300);
+
+    setTimeout(function () {
+      this.$titleInner.text(this.words[this.index]);
+    }.bind(this), 1700);
+
+    setTimeout(function () {
+      this.$titleBorder.css({
+        'transition':'1s padding-left ease-in-out',
+        'padding-top':'10px',
+        'padding-bottom':'10px',
+        'padding-left':this.$titleContainer.width()
+      })
+      this.index++;
+    }.bind(this), 1000);
+  };
+}
+
+
+
+$(function() {
+  //config
+  $.scrollify({
+    section : ".section",
+    scrollSpeed: 500,
+  });
+
+  var $hamburger = $('.hamburger');
+
+  var title = new Title(['test1','teeeeeeeeeest2','test3']);
+  setInterval(function () {
+    title.slide();
+  }, 5000);
+
+  $hamburger.click(function (e) {
+    e.preventDefault();
+    if ($hamburger.hasClass('is-active')) {
+      $hamburger.removeClass('is-active')
+    } else {
+      $hamburger.addClass('is-active')
+    }
+  })
+});
