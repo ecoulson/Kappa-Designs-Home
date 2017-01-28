943d3b078a2a14411ff978762c145d7ba3f1e1f3
unholydeath785 <evan@coulson-orellana.com>
2017-01-27 21:58:24 -0800

    load push

--- js/client.js
+++ js/client.js
@@ -2,7 +2,7 @@ function Title(words) {
   this.words = words;
   this.index = 0;
   this.$titleContainer = $('.desc-title');
-  this.$titleInner = $('.desc-animated');
+  this.$titleInner = $('.desc-animated')
   this.$titleInner.text(this.words[this.index]);
 
   this.slide = function () {
@@ -33,6 +33,7 @@ $(function() {
   $.scrollify({
     section : ".section",
     scrollSpeed: 600,
+    standardScrollElements: ".sroll"
   });
 
   var $hamburger = $('.hamburger');
@@ -40,12 +41,45 @@ $(function() {
   var $card = $('#card');
   var $cardFlip = $('#email-button');
   var $emailFlip = $('#card-button');
+  var $menu = $('.flex-menu');
+  var $missionBtn = $('.see-mission');
+  var $backBtn = $('.back-btn');
 
   var title = new Title(['Kappa','meme','design']);
+  var keys = {
+    37: 1,
+    38: 1,
+    39: 1,
+    40: 1,
+  }
 
   setInterval(function () {
     title.slide();
   }, 5000);
+
+  $(window).resize(function () {
+    $(".clear-fix-about").css({
+      'padding-bottom':100
+    })
+  })
+
+  $missionBtn.click(function (e) {
+    $(this).parent().css({
+      'transition':'margin-left 0.5s ease',
+      'margin-left':'-45%'
+    })
+  })
+
+  $backBtn.click(function (e) {
+    $missionBtn.parent().css({
+      'margin-left':'5%'
+    })
+  })
+
+  $menu.click(function (e) {
+    $overlay.slideUp();
+    $hamburger.removeClass('is-active')
+  })
 
   $cardFlip.click(function (e) {
     $card.addClass('flipped');
@@ -57,7 +91,6 @@ $(function() {
   })
 
   $hamburger.click(function (e) {
-    e.preventDefault();
     if ($hamburger.hasClass('is-active')) {
       $hamburger.removeClass('is-active')
       $overlay.slideUp();
@@ -67,25 +100,20 @@ $(function() {
     }
   })
 
-  $overlay.click(function (e) {
-    $overlay.slideUp();
-    $hamburger.removeClass('is-active')
-  })
-});
-
-function smoothScool (duration) {
-  $('a[href^="#"]').on('click', function(event) {
-    if ($($(this)).prop('id') != '#next' && $($(this)).prop('id') != '#prev') {
-      var target = $( $(this).attr('href') );
-      if( target.length ) {
-        event.preventDefault();
-        $('html, body').animate({
-            scrollTop: target.offset().top
-        }, duration);
+  function smoothScool (duration) {
+    $('a[href^="#"]').on('click', function(event) {
+      if ($($(this)).prop('id') != '#next' && $($(this)).prop('id') != '#prev') {
+        var target = $( $(this).attr('href') );
+        if( target.length ) {
+          event.preventDefault();
+          $('html, body').animate({
+              scrollTop: target.offset().top
+          }, duration);
+        }
       }
-    }
-  });
-}
+    })
+  }
+})
 
 function sendMail() {
   var emailString = 'mailto:kappadesignsco@gmail.com?subject='+$('.subject-input').val()+'&body='+$('#email-content').val()+'&cc='+$('.cc-input').val()+'';
